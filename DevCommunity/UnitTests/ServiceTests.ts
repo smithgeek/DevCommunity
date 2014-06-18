d8b3878cd364175de6835597facb6b8b06887b89
brentosmith <brent@smithgeek.com>
2014-06-17 22:03:51 -0500

    Added a few unit tests.

--- DevCommunity/UnitTests/ServiceTests.ts
+++ DevCommunity/UnitTests/ServiceTests.ts
@@ -3,10 +3,41 @@
 
 QUnit.module("UserSvc");
 
-test("1", function ( assert ) {
-    assert.equal(true, true);
+function getUserSvc(storage) {
+    return new UserSvc(storage);
+}
+
+function getUserSvcWithoutKey() {
+    return getUserSvc({ get: function (key) { return null; } });
+}
+
+function getUserSvcWithKey() {
+    return getUserSvc({ get: function (key) { return { email: 'me@someplace.com' }; } });
+}
+
+test("EmailNotInLocalStorage", function (assert) {
+    equal(getUserSvcWithoutKey().getUser(), '');
 });
 
-test("fail", function () {
-    equal(true, false);
+test("EmailInLocalStorage", function () {
+    equal(getUserSvcWithKey().getUser(), 'me@someplace.com');
+});
+
+test("UserLoggedIn", function (assert) {
+    equal(getUserSvcWithKey().isLoggedIn(), true);
+});
+
+test("UserNotLoggedIn", function (assert) {
+    equal(getUserSvcWithoutKey().isLoggedIn(), false);
+});
+
+test("UserLogsOut", function (assert) {
+    var count = 0;
+    var localStorage = {
+        clearAll: function () {
+            count++;
+        }
+    };
+    getUserSvc(localStorage).logOut()
+    equal(count, 1);
 });
