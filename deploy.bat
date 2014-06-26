33f9c7a6d9f64bac8d8f757a3b2f19239e0e4c67
brentosmith <brent@smithgeek.com>
2014-06-25 23:12:07 -0500

    Remove *.js.map files and added deploy.bat.

---
+++ deploy.bat
@@ -0,0 +1,8 @@
+@echo off
+xcopy DevCommunity\public %1\public /i /s /Y /d
+xcopy DevCommunity\node_modules %1\node_modules /i /s /Y /d
+xcopy DevCommunity\routes %1\routes /i /s /Y /d
+xcopy DevCommunity\views %1\views /i /s /Y /d
+xcopy DevCommunity\views %1\views /i /s /Y /d
+copy DevCommunity\config.js %1\config.js /d
+copy DevCommunity\server.js %1\server.js /d
