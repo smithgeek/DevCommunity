9ddd23b1fe75b27643a74d3cb551f07027d6b514
Brent Smith <brent.smith@garmin.com>
2014-06-26 08:49:36 -0500

    Don't copy config.js on deploy.
    Change-Id: Iea7178d5a070109e27d50d2e1241d8e120003d09

--- deploy.bat
+++ deploy.bat
@@ -4,5 +4,4 @@ xcopy DevCommunity\node_modules %1\node_modules /i /s /Y /d
 xcopy DevCommunity\routes %1\routes /i /s /Y /d
 xcopy DevCommunity\views %1\views /i /s /Y /d
 xcopy DevCommunity\views %1\views /i /s /Y /d
-copy DevCommunity\config.js %1\config.js /d
 copy DevCommunity\server.js %1\server.js /d
