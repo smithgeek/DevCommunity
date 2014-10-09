a6b113c37d975eed3cea905cf50569a38d72fae5
Brent Smith <brent.smith@garmin.com>
2014-10-09 15:09:51 -0500

    Recreated ts files from js files because they were accidentally deleted.
    Change-Id: I4c852adff49f429a802613d545009870c91a0b52

---
+++ DevCommunity/source/Client/CommentController.ts
@@ -0,0 +1,56 @@
+﻿///ts:import=app
+import app = require('./app');
+
+class CommentController {
+    constructor(private $scope, private $http) {
+        this.$scope.isSubscribed = true;
+        this.$scope.commentGroup = { dataId: '', subscribers: [], comments: null };
+        this.$scope.$watch('commentId', function (value) {
+            if (value) {
+                //$http.get('/api/GetComments/' + $scope.commentId).success((data: CommentGroup) => {
+                //});
+                var c = {
+                    time: new Date(Date.now()),
+                    author: "brent",
+                    replies: [],
+                    votesUp: [],
+                    votesDown: [],
+                    data: "some comment stuff",
+                    id: "1",
+                    hierarchy: 0
+                };
+                var d = {
+                    time: new Date(Date.now()),
+                    author: "brent",
+                    replies: [],
+                    votesUp: [],
+                    votesDown: [],
+                    data: "some more comment stuff",
+                    id: "2",
+                    hierarchy: 1
+                };
+                var e = {
+                    time: new Date(Date.now()),
+                    author: "brent",
+                    replies: [],
+                    votesUp: [],
+                    votesDown: [],
+                    data: "even more comment stuff",
+                    id: "3",
+                    hierarchy: 0
+                };
+                c.replies.push(d);
+                this.$scope.commentGroup = { dataId: $scope.commentId, subscribers: [], comments: [c, e] };
+            }
+        });
+    }
+    
+    public toggleSubscribe() {
+        this.$scope.isSubscribed = !this.$scope.isSubscribed;
+    }
+}
+
+angular.module(app.getModuleName()).controller('CommentController', ['$scope', '$http', CommentController]);
+
+exports = CommentController;
+
---
+++ DevCommunity/source/Client/Impl/MyCommentsDirective.ts
@@ -0,0 +1,37 @@
+﻿///ts:import=app
+import app = require('../app'); ///ts:import:generated
+
+(function () {
+    var appModule = angular.module(app.getModuleName());
+
+    appModule.directive('myComments', function () {
+        return {
+            restrict: 'E',
+            templateUrl: '/partials/CommentSystem.html',
+            controller: [
+                '$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
+                    $attrs.$observe('commentId', function (value) {
+                        if (value && value[value.length - 1] != '-') {
+                            $scope.commentId = value;
+                        }
+                    });
+                }]
+        };
+    });
+
+    appModule.directive('myComment', [
+        '$compile', '$timeout', function ($compile, timer) {
+            return {
+                restrict: 'E',
+                templateUrl: 'partials/Comment.html',
+                link: function (scope, element, attrs) {
+                    element.append($compile('<my-comment ng-repeat="comment in comment.replies" />')(scope));
+                    var timeagoify = function () {
+                        $("time.timeago").timeago();
+                    };
+                    timer(timeagoify, 0);
+                }
+            };
+        }]);
+})();
+//# sourceMappingURL=MyCommentsDirective.js.map
